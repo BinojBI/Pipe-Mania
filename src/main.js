4a3a5f5dfbccaee8d0a6414389360924058debba
BINOJ\binoj <binojishara21@gmail.com>
2025-11-12 19:34:22 +0000

    fixed game title, refactored code

--- src/main.js
+++ src/main.js
@@ -1,30 +1,24 @@
-import { GameScene } from './scenes/GameScene.js';
-import { MainMenu } from './scenes/MainMenu.js';
+import { GameScene } from "./scenes/GameScene.js";
+import { MainMenu } from "./scenes/MainMenu.js";
 
 const config = {
-    type: Phaser.AUTO,
-    title: 'Overlord Rising',
-    description: '',
-    parent: 'game-container',
-    width: 1280,
-    height: 720,
-    backgroundColor: '#fff',
-    pixelArt: false,
-    scene: [
-        MainMenu,
-        GameScene 
-    ],
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-     physics: {
+  type: Phaser.AUTO,
+  title: "Pipe Mania",
+  description: "",
+  parent: "game-container",
+  width: 1280,
+  height: 720,
+  backgroundColor: "#fff",
+  pixelArt: false,
+  scene: [MainMenu, GameScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
     default: "arcade",
-    arcade: {
-        
-    }
-    }
-}
+    arcade: {},
+  },
+};
 
 new Phaser.Game(config);
-            
